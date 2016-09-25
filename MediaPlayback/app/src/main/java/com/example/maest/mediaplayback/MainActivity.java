6e6d46a2e4fb4b501b12c2ca7e48ca747fb52366
Maestro5563 <maestro5563@gwu.edu>
2016-09-24 23:25:39 -0400

    New update

--- MediaPlayback/app/src/main/java/com/example/maest/mediaplayback/MainActivity.java
+++ MediaPlayback/app/src/main/java/com/example/maest/mediaplayback/MainActivity.java
@@ -6,19 +6,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-    MediaPlayer mPlayer;
+    MediaPlayer mPlayer;//This is MediaPlayer class inside the feature Media Playback.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);//Obviously, this activates and displays the UI.
     }
-    public void playButton (View v){
-        mPlayer = MediaPlayer.create(this, R.raw.pop);
-        mPlayer.start();// no need to call prepare(); create() does that for you
+    public void playButton (View v){//Triggers when play button is clicked.
+        mPlayer = MediaPlayer.create(this, R.raw.pop);//Create and set the default music in raw resource into mPlayer.
+        mPlayer.start();//No need to call prepare(), create() does that for you. Now the music starts playing.
     }
-    public void stopButton (View v) {
-
+    public void stopButton (View v) {//Triggers when stop button is clicked
         if(mPlayer!=null && mPlayer.isPlaying()){//If music is playing already
             mPlayer.stop();//Stop playing the music
         }
