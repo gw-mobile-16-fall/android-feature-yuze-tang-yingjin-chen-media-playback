4a5309f568b4882fd16336e3b89006ac30dcba39
everestsky <YingjinChen@YingjindeMacBook-Pro.local>
2016-09-24 23:03:46 -0400

    update some files

--- MediaPlayback/app/src/main/java/com/example/maest/mediaplayback/MainActivity.java
+++ MediaPlayback/app/src/main/java/com/example/maest/mediaplayback/MainActivity.java
@@ -1,22 +1,26 @@
 package com.example.maest.mediaplayback;
 
-import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    MediaPlayer mPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void playButton (View v){
+        mPlayer = MediaPlayer.create(this, R.raw.pop);
+        mPlayer.start();// no need to call prepare(); create() does that for you
+    }
+    public void stopButton (View v) {
 
-
-        MediaPlayer media = new MediaPlayer();
-
-
-        media.start();
+        if(mPlayer!=null && mPlayer.isPlaying()){//If music is playing already
+            mPlayer.stop();//Stop playing the music
+        }
     }
 }
